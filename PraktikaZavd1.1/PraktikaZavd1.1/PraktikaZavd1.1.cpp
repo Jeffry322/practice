c7cc5933f456c32dbae353d283b8161a1eed53c4
Jeffry322 <67588481+Jeffry322@users.noreply.github.com>
2020-06-29 18:22:29 +0300

    Update PraktikaZavd1.1.cpp

--- PraktikaZavd1.1/PraktikaZavd1.1/PraktikaZavd1.1.cpp
+++ PraktikaZavd1.1/PraktikaZavd1.1/PraktikaZavd1.1.cpp
@@ -6,8 +6,8 @@ int main() {
 	int lenght;
 	int x = 0,b = 1;
 	cin >>lenght;
-	int *a = new int[lenght];
-	for (int i = 0; i < lenght; i++)
+	int *a = new int[lenght]; //ствоорюємо та заповнюємо динамічний масив
+	for (int i = 0; i < lenght; i++) 
 	{
 		cout << "Enter number"<<" "<< i << endl;
 		cin >> a[i];
@@ -15,15 +15,15 @@ int main() {
 	}
 	for (int i = 0; i < lenght; i++)
 	{
-		if (a[b] > a[i])
-		{
+		if (a[b] > a[i])          //якщо елемент  a[b] (який завжди більший елемента a[i] на одиницю) більший за а[i], то до лічильник х додаємо +1
+		{                         
 			x++;
 		}
 		b++;
 	}
 	x++;
-	if (lenght == x)
-		cout << "Your List Is Ascending";
+	if (lenght == x)                                     //якщо кількість разів скільки попередній елемент був менший наступний співпадає з кількістю
+		cout << "Your List Is Ascending";            //елементів в масиві, то послідновніть є зростаючею
 	else
 		cout << "Your List isn't Ascending" << endl;
 }
